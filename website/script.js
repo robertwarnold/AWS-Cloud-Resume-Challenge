16350ba31c90242a37d712507f7146879fcd1a0d
robertwarnold <robertwarnold13@gmail.com>
2023-07-12 18:00:44 -0400

    Added infra code

--- website/script.js
+++ website/script.js
@@ -59,7 +59,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://vi7rlvoxcuzguufofxacthv3bq0gbhqu.lambda-url.us-east-1.on.aws/"
+        "https://555kekzoa7t4kh72jydwuqrua40sxlvc.lambda-url.us-east-1.on.aws/"
     );
     let data = await response.json();
     counter.innerHTML = `👀 Views: ${data}`;
